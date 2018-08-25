045aafebc9c73298e8eaa4f8a7df8b163fa77449
hesslau <henrik@hesslau.org>
2018-08-25 12:22:57 +0200

    add laravel/passport for oauth

--- config/app.php
+++ config/app.php
@@ -160,6 +160,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * OAuth
+         */
+        Laravel\Passport\PassportServiceProvider::class,
+
     ],
 
     /*
